283023dfeac93d1b8eb7648bb92e56cfd13f583b
Sabryna <scullysabrina8@gmail.com>
2022-02-20 23:42:48 -0300

    Comentários

--- src/DesafioSenha.java
+++ src/DesafioSenha.java
@@ -4,15 +4,17 @@ public class DesafioSenha {
 	public static String mensageDesuc = "";
 
 	public static void main(String[] args) {
-
+   // chama  a o método cadastroSenha
 		cadastroSenha();
 
 	}
-
+       // criando o método
 	public static void cadastroSenha() {
 
+	     // entrada
 		Scanner entrada = new Scanner(System.in);
 
+		// menu
 		System.out.println("------------------------------------------------------------");
 		System.out.println("Entre com a sua senha :" + "\n" + "* A senha precisa ter 6 digitos ou mais." + "\n"
 				+ "* conter um digito." + "\n" + "* conter uma letra em maiúsculo." + "\n"
@@ -20,7 +22,7 @@ public class DesafioSenha {
 
 		System.out.print("Digite a senha: ");
 		String senha = entrada.nextLine();
-
+        // método que valida a senha
 		ValidatePassword(senha);
 
 		entrada.close();
@@ -29,7 +31,7 @@ public class DesafioSenha {
 	public static void ValidatePassword(String senha) {
 
 		int validarChar = 0;
-
+ // verifica se está entre o minimo de digitos
 		if (senha.length() < 6) {
 			validarChar = 6 - senha.length();
 		} else {
@@ -47,7 +49,7 @@ public class DesafioSenha {
 				validarChar++;
 			}
 		}
-
+// imprime erro se não tiver com os requisitos para contruir a senha 
 		System.out.printf("\n  Caracteres restantes para sua senha requisitos de segurança :  faltam  %s Caracteres ",
 				validarChar);
 
